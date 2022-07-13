f1f11b2a61019411b2e23a95e6be6511656a4615
Ryan Ramlagan <rramlagan12@gmail.com>
2022-07-13 19:55:11 -0400

    first commit

---
+++ Personal-Project/street_scene_blog/android/app/src/main/kotlin/com/example/street_scene_blog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.street_scene_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- steet_scene_blog/android/app/src/main/kotlin/com/example/steet_scene_blog/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.steet_scene_blog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
